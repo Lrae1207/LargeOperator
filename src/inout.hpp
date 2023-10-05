4616d911865066f8c82bafa8495c076c90c82e8f
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-10-05 12:53:05 -0700

    Fixed inout.hpp
    Accidentally put inout.cpp here.

--- src/inout.hpp
+++ src/inout.hpp
@@ -1,32 +1,14 @@
-#include "inout.hpp"
+#include <iostream>
+#include <string>
 
-//https://stackoverflow.com/questions/478898/how-do-i-execute-a-command-and-get-the-output-of-the-command-within-c-using-po
-std::string exec(const char* cmd) {
-    char buffer[128];
-    std::string result = "";
-    FILE* pipe = popen(cmd, "r");
-    if (!pipe) throw std::runtime_error("popen() failed!");
-    try {
-        while (fgets(buffer, sizeof buffer, pipe) != NULL) {
-            result += buffer;
-        }
-    } catch (...) {
-        pclose(pipe);
-        throw;
-    }
-    pclose(pipe);
-    return result;
-}
+#define BLACK     "\x1b[30m"
+#define RED       "\x1b[31m"
+#define GREEN     "\x1b[32m"
+#define YELLOW    "\x1b[33m"
+#define BLUE      "\x1b[34m"
+#define MAGENTA   "\x1b[35m"
+#define CYAN      "\x1b[36m"
+#define RESET     "\x1b[37m"
 
-// Print n characters of buffer and fill any remaining space with ' 's
-void printClamped(std::string buffer, int n, std::string color) {
-    std::cout << color;
-    for (int i = 0; i < n; ++i) {
-        if (i < buffer.size()) {
-            std::cout << buffer[i];
-        } else {
-            std::cout << ' ';
-        }
-    }
-    std::cout << RESET;
-}
+std::string exec(const char* cmd);
+void printClamped(std::string buffer, int n, std::string color);
