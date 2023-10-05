4202bc22ea89e6f06e1ca31f4bd1cb67e1777f20
Lrae <109258251+Lrae1207@users.noreply.github.com>
2023-10-05 12:50:38 -0700

    Create src/memop.hpp

---
+++ src/memop.hpp
@@ -0,0 +1,7 @@
+#include <string>
+#include <vector>
+
+std::string memToStr(char *c, size_t n);
+std::string xorStrEx(std::string op1, std::string op2);
+std::string orStrEx(std::string op1, std::string op2);
+std::string andStrEx(std::string op1, std::string op2);
